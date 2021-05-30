21befd188fb3520d07ed283a9bfed4cea5100d5d
ashimsaini <ashimsaini37@gmail.com>
2021-05-30 21:38:25 +0530

    Main UI

--- app/src/main/java/com/design/quickexamprep/MainActivity.java
+++ app/src/main/java/com/design/quickexamprep/MainActivity.java
@@ -1,8 +1,12 @@
 package com.design.quickexamprep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomeFragment mHomeFragment = new HomeFragment();
+
+        setCurrentFragment(mHomeFragment);
+        ((BottomNavigationView) findViewById(R.id.main_bottom_navigation)).setOnNavigationItemReselectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.my_home:
+                case R.id.my_package:
+                case R.id.my_test:
+                    setCurrentFragment(mHomeFragment);
+                    break;
+
+            }
+        });
     }
+
+    private void setCurrentFragment(HomeFragment mHomeFragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, mHomeFragment).commit();
+    }
+
+
 }
