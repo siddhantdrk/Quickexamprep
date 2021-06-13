9cce81023f8cf705a399ee01fe8c37e9f90684f9
ashimsaini <ashimsaini37@gmail.com>
2021-06-13 20:46:25 +0530

    drawer setup

--- app/src/main/java/com/design/quickexamprep/QuestionScreen.java
+++ app/src/main/java/com/design/quickexamprep/QuestionScreen.java
@@ -2,9 +2,13 @@ package com.design.quickexamprep;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,11 +19,25 @@ import java.util.ArrayList;
 public class QuestionScreen extends AppCompatActivity {
 
     Activity activity = QuestionScreen.this;
+    DrawerLayout drawer_layout;
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
+
+        drawer_layout = findViewById(R.id.drawer_layout);
+        drawer_layout.setDrawerListener(drawerToggle);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawer_layout,
+                null, R.string.app_name, R.string.app_name);
+        //This is necessary to change the icon of the Drawer Toggle upon state change.
+        drawerToggle.syncState();
+
+        findViewById(R.id.iv_drawer).setOnClickListener(view -> {
+            drawer_layout.openDrawer(Gravity.RIGHT);
+        });
 
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("મુઝફ્ફર ખાનને");
